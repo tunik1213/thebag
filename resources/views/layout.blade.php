14b227ad2eff4eaf8d5df1491fef0439c38e7b5b
tunik <tunik@detali.zp.ua>
2024-08-29 10:04:51 +0300

    Here we go!

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="ua">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>The bag</title>
+
+    <link href="/bootstrap.min.css" rel="stylesheet">
+    <link href="app.css" rel="stylesheet">
+  </head>
+  <body>
+    
+    <header>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <div id="header-container" class="container">
+          <a class="navbar-brand" href="/"><img id="logo-image" src="/logo.png" /></a>
+          <span id="header-slogan">Сумка - твій всесвіт!</span>
+        </div>
+        
+      </nav>
+    </header>
+
+    <main role="main" class="">
+
+        @yield('content', 'Default content')
+
+    </main>
+
+    <footer class="footer">
+      <div class="container">
+        <span class="text-muted">footer content here.</span>
+      </div>
+    </footer>
+
+    <script src="/jquery-3.3.1.slim.min.js"></script>
+    <script src="/bootstrap.min.js"></script>
+    <script src="/popper.min.js"></script>
+  </body>
+</html>
