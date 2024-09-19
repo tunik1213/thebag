0a7630c2ee67b2b091ba47bfa98dbc3cf81d0f65
tunik <tunik@detali.zp.ua>
2024-09-19 13:21:12 +0300

    favicon created

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,8 +3,13 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="{{$viewdata['description'] ?? 'THE BAG - сумки ручної роботи з еко-шкіри'}}">
+    <meta name="description" content="{{$viewdata['description'] ?? 'THE BAG - сумки ручної роботи'}}">
     <meta name="author" content="">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
 
     <title>{{$viewdata['title'] ?? 'THE BAG - Та сама сумка'}}</title>
 
